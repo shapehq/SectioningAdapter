18a4945afcc213dd1e18212b548c3aaf15ca6964
Mikkel Schläger <mikkel@novasa.com>
2020-06-14 16:51:02 +0200

    mockk

--- app/src/androidTest/java/com/novasa/sectioningadapterexample/injection/MockModule.kt
+++ app/src/androidTest/java/com/novasa/sectioningadapterexample/injection/MockModule.kt
@@ -3,11 +3,11 @@ package com.novasa.sectioningadapterexample.injection
 import com.novasa.sectioningadapterexample.data.DataSource
 import dagger.Module
 import dagger.Provides
-import org.mockito.Mockito
+import io.mockk.mockk
 
 @Module
 class MockModule {
 
     @Provides
-    fun provideDataSource(): DataSource = Mockito.mock(DataSource::class.java)
+    fun provideDataSource(): DataSource = mockk()
 }
